0a465a408185044b40883106eb44a8f9c79a9365
Miutaku <miutaku@miutakuMacBook-Pro.local>
2024-12-13 04:39:48 +0900

    notify.shのWebhook処理を改善し、config.ymlに新しいWebhook設定を追加

--- epgstation/config/notify.sh
+++ epgstation/config/notify.sh
@@ -1,4 +1,32 @@
-#!/bin/sh
-message=$1
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message $NAME\"}" __NOTIFY_WEBHOOK__
+#!/usr/bin/bash
+WEBHOOK_URL=$1
+MESSAGE=$2
+TITLENAME=$3
 
+# 投稿テスト
+curl -s -i -H "Content-Type: application/json" --data-binary @- "$WEBHOOK_URL" <<EOF
+{
+   "type":"message",
+   "attachments":[
+      {
+         "contentType":"application/vnd.microsoft.card.adaptive",
+         "contentUrl":null,
+         "content":{
+            "\$schema":"http://adaptivecards.io/schemas/adaptive-card.json",
+            "type":"AdaptiveCard",
+            "version":"1.4",
+            "body":[
+               {
+                  "type": "TextBlock",
+                  "text": "$MESSAGE"
+               },
+               {
+                  "type": "TextBlock",
+                  "text": "$TITLENAME"
+               }
+            ]
+         }
+      }
+   ]
+}
+EOF
